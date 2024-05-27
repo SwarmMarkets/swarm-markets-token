6e465c2717b0d2bb111e03cb1b70da5d6c570195
big B <57547705+b0gdaniy@users.noreply.github.com>
2024-05-27 13:31:45 +0300

    upd: SMT

--- contracts/SwarmMarketsToken.sol
+++ contracts/SwarmMarketsToken.sol
@@ -4,18 +4,16 @@ pragma solidity ^0.7.0;
 import "@openzeppelin/contracts/presets/ERC20PresetFixedSupply.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SwarmMarketsToken is ERC20PresetFixedSupply, Ownable{
-    
+contract SwarmMarketsToken is ERC20PresetFixedSupply, Ownable {
     string public KYA;
 
     /**
      * See {ERC20PresetFixedSupply-constructor}.
      */
     // solhint-disable-next-line no-empty-blocks
-    constructor(address owner) 
-        ERC20PresetFixedSupply("Swarm Markets", "SMT", 250000000 * 10**18, owner)
-        Ownable() 
-            {}
+    constructor(
+        address _owner
+    ) ERC20PresetFixedSupply("Swarm Markets", "SMT", 250000000 * 10 ** 18, _owner) Ownable() {}
 
     function setKYA(string calldata _knowYourAsset) external onlyOwner {
         KYA = _knowYourAsset;
