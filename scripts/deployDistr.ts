456f673e26c505383924db9c0de43673a2056ef7
Bohdan Pukhno <pukhnobg@gmail.com>
2025-10-08 17:17:36 +0300

    deploy: plasma

--- scripts/deployDistr.ts
+++ scripts/deployDistr.ts
@@ -1,7 +1,7 @@
 import { ethers } from 'hardhat';
 
-const smt = '0x4d544E1236D1D36fCcD0bA31E5c30FC5cFB4FF4a';
-const owner = '0x1dEE90df6cDd8a7dA3510FB5b0305EFE57239641';
+const smt = '';
+const owner = '';
 
 async function main() {
   const factory = await ethers.getContractFactory('SmtDistributor');
--- scripts/verifyDistr.ts
+++ scripts/verifyDistr.ts
@@ -1,8 +1,8 @@
 import hre from 'hardhat';
 
-const distr = '0xbCdEb213c74B0a1BD3586DFa07Ba4107470A0e18';
-const smt = '0x4d544E1236D1D36fCcD0bA31E5c30FC5cFB4FF4a';
-const owner = '0x1dEE90df6cDd8a7dA3510FB5b0305EFE57239641';
+const distr = '';
+const smt = '';
+const owner = '';
 
 async function main(): Promise<void> {
   await hre.run('verify:verify', {
